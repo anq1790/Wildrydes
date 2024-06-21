3500ee7d996444a1637bdba00ca1b44d358fb66f
anq1790 <dholeanushka9@gmail.com>
2024-06-20 17:25:30 -0700

    Add files via upload

---
+++ config (4).js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_V9PVlqTq8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2osuhpu1ao4pe91etqi5urom89', // e.g. 2osuhpu1ao4pe91etqi5urom89
+        region: 'us-west-2' // e.g. us-west-2
+    },
+    api: {
+        invokeUrl: 'https://g6amelshl3.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://g6amelshl3.execute-api.us-west-2.amazonaws.com/prod
+    }
+};
